d1eb0cc7bb3c033e44f1d5d5d79ad1b765430d5e
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-07-15 13:54:17 +0400

    master test

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>zd</h1>
-      <h1>zd 2</h1>
-      <h1>zd 3</h1>
-      <h1>zd 4 test</h1>
-      <h1>zd 4 test igivea</h1>
-      <h1>zd 5 test igivea</h1>
-      <h1>zd 5 test igivea es masteris</h1>
-      <h1>zd 5 test igivea es masteris</h1>
+     <h1>masteris testi</h1>
     </div>
   );
 }
