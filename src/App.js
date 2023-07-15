f6a2cbe94b0e698f2e8be9e8f616235a4dcb09cb
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-07-15 13:46:25 +0400

    Initializing temporary branch

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>zd</h1>
+      <h1>zd 2</h1>
+      <h1>zd 3</h1>
+      <h1>zd 4 test</h1>
+      <h1>zd 4 test igivea</h1>
+      <h1>zd 5 test igivea</h1>
+    </div>
+  );
+}
+
+export default App;
