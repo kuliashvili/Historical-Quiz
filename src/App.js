384c00a5f2f7ddab6652736e86c0a821704e962c
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-07-20 22:08:30 +0400

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
+import ExchangeRate from './components/ExchangeRate';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>masteris testi</h1>
-     <h1>masteris testi</h1>
-     <h1>masteris testi</h1>
-     <h1>masteris testi bl</h1>
+    <h1>zd</h1>
+    <ExchangeRate />
     </div>
+
+    
   );
 }
 
