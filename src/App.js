3ecc76816378cd9ed0cbdc59ae7f14b7566586f1
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-07-21 19:02:08 +0400

    upd final

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
      <h1>masteris testi</h1>
      <h1>masteris testi</h1>
      <h1>masteris testi</h1>
      <h1>masteris testi</h1>
      <h1>masteris testi</h1>
      <h1>masteris testi 11111</h1>
-=======
+
     <h1>zd</h1>
     <ExchangeRate />
->>>>>>> gh-pages
+
     </div>
 
     
