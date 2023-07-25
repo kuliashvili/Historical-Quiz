281c032d9bdbd43cb4ab5a285f2aae145fab46ac
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-07-25 16:56:33 +0400

    upd master

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import logo from './logo.svg';
-import ExchangeRate from './components/ExchangeRate';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-     <h1>masteris testi</h1>
-     <h1>masteris testi</h1>
-     <h1>masteris testi</h1>
-     <h1>masteris testi</h1>
-     <h1>masteris testi</h1>
-     <h1>masteris testi 11111</h1>
-
-    <h1>zd</h1>
-    <ExchangeRate />
+<h1>georgian history quiz</h1>
+   
 
     </div>
 
