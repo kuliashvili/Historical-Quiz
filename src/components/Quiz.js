2dabd012eaf065727c5058af1532dea7b3904893
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-07-25 17:27:58 +0400

    added finished component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,6 +3,16 @@ import "./Quiz.css"
 
 export default function Quiz() {
   return (
-    <div className='quiz'>Quiz</div>
+    <div className='quiz'>
+        <div className="question">
+            <div className="question-header">romel wels moxda didgori</div>
+            <button className="question-answer-1">1121</button>
+            <button className="question-answer-2">1135</button>
+            <button className="question-answer-3">456</button>
+            <button className="question-answer-4">1907</button>
+        </div>
+
+        <button className='finish'>finish quiz</button>
+    </div>
   )
 }
