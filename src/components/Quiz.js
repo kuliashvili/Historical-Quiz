83e4468b2dccfe5189277a5a256e89ac8c2dde4e
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-07-28 18:45:49 +0400

    update function

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,75 +1,78 @@
-import React from 'react'
-import "./Quiz.css"
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import './Quiz.css';
 
 export default function Quiz() {
-
   const quizData = [
     {
-      question: "rodis moxda didgoris brdzola?",
-      answers: ['1121', '3324', '531', '125'],
-     
+      question: 'The Georgian national epic, "The Knight in the Panthers Skin," was written by:',
+      answers: ['Shota Rustaveli', ' Vazha-Pshavela', ' Ilia Chavchavadze', 'Nikoloz Baratashvili'],
+      correctAnswer: 'Shota Rustaveli',
     },
     {
-      question: "ra feria pepela?",
-      answers: ['red', 'blue', 'gray', 'black'],
-      
+      question: 'Which Georgian historical figure is associated with the invention of the Georgian alphabet?',
+      answers: ['Saint Nino', 'King Parnavaz', 'King Mirian III', 'King Vakhtang I Gorgasali'],
+      correctAnswer: 'King Parnavaz',
     },
     {
-      question: "qatami tu kvercxi",
-      answers: ['qatami', 'dinozavri', 'rdze', 'kvercxi'],
-      
+      question: 'In which battle did King David IV (the Builder) defeat the Seljuk Turks, securing Georgian independence?',
+      answers: ['Battle of Didgori', 'Battle of Martqopi', 'Battle of Lake Paravani', 'Battle of Krtsanisi'],
+      correctAnswer: 'Battle of Didgori',
     },
     {
-      question: "kaibichoba tu torti",
-      answers: ['torti', 'shorti', 'porti', 'golfi'],
-      
+      question: 'Which Georgian king is often referred to as "George the Brilliant" due to his patronage of the arts and culture?',
+      answers: ['George V of Georgia', 'Giorgi IV (Lasha)', 'George IX of Kartli', 'George II of Imereti'],
+      correctAnswer: 'George V of Georgia',
+    },
+    {
+      question: 'What was the name of the prominent Georgian queen who ruled in the 12th century and played a significant role in expanding Georgias territory?',
+      answers: ['Queen Tamar', 'Queen Rusudan', ' Queen Ketevan', 'Queen Marta of Margveti'],
+      correctAnswer: 'Queen Tamar',
+    },
+    
+  ];
+
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const questionData = quizData[questionIndex];
+
+  const handleAnswerClick = (selectedAnswer) => {
+    // Check if the selected answer is correct
+    if (selectedAnswer === questionData.correctAnswer) {
+      // If the answer is correct, increment the score
+      setScore((prevScore) => prevScore + 1);
     }
-  ]
 
-const [questionIndex, setQuestionIndex] = useState(0)
+    // Move to the next question
+    if (questionIndex < quizData.length - 1) {
+      setQuestionIndex(questionIndex + 1);
+    } else {
+      // Quiz finished, display the score
+     
+    }
+  };
 
-const questionData = quizData[questionIndex]
-
-const rightAnswer1 = "1121"
-const rightAnswer2 = "black"
-const rightAnswer3 = "dinozavri"
-const rightAnswer4 = "shorti"
-
-const checkRightAnswer = (event) => {
-  const buttonText = event.target.textContent;
-  if (buttonText === '1121') {
-    event.target.style.backgroundColor = 'red';
+  const show = () => {
+    alert(`Quiz finished. Your score: ${score}/${quizData.length}`);
   }
-};
-
-const handleNextQuestion = () => {
-
-  if(questionIndex < quizData.length -1){
-    setQuestionIndex(questionIndex + 1)
-  }else{
-    alert("quiz finished")
-  }
-
-}
-
- 
 
   return (
-    <div className='quiz'>
-      <h1>aaaaaaaaa</h1>
-        <div className="question">
-            <div className="question-header">{questionData.question}</div>
-            <div className="question-list">
-            <button onClick={checkRightAnswer} className="quest answer-1">{questionData.answers[0]}   </button>
-            <button onClick={checkRightAnswer} className="quest answer-2">{questionData.answers[1]}   </button>
-            <button onClick={checkRightAnswer} className="quest answer-3">{questionData.answers[2]}   </button>
-            <button onClick={checkRightAnswer} className="quest answer-4">{questionData.answers[3]}   </button>
-            </div>
-            
+    <div className="quiz">
+      <div className="question">
+        <div className="question-header">{questionData.question}</div>
+        <div className="question-list">
+          {questionData.answers.map((answer, index) => (
+            <button
+              key={index}
+              className="quest"
+              onClick={() => handleAnswerClick(answer)}
+            >
+              {answer}
+            </button>
+          ))}
         </div>
+      </div>
 
-        <button onClick={handleNextQuestion} className='finish'>{questionIndex < quizData.length -1 ? "Next Question" : "Finish"}</button>
+ <button onClick={show} className="finish">Finish</button>
     </div>
-  )
+  );
 }
