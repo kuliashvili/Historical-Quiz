69923fb11fd1a796f57cc702fbd5dea6430fc2a4
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-07-28 16:44:43 +0400

    updt master

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -8,22 +8,22 @@ export default function Quiz() {
     {
       question: "rodis moxda didgoris brdzola?",
       answers: ['1121', '3324', '531', '125'],
-      rightAnswer: '1121'
+     
     },
     {
       question: "ra feria pepela?",
       answers: ['red', 'blue', 'gray', 'black'],
-      rightAnswer: 'black'
+      
     },
     {
       question: "qatami tu kvercxi",
       answers: ['qatami', 'dinozavri', 'rdze', 'kvercxi'],
-      rightAnswer: 'dinozavri'
+      
     },
     {
       question: "kaibichoba tu torti",
       answers: ['torti', 'shorti', 'porti', 'golfi'],
-      rightAnswer: 'shorti'
+      
     }
   ]
 
@@ -31,22 +31,45 @@ const [questionIndex, setQuestionIndex] = useState(0)
 
 const questionData = quizData[questionIndex]
 
+const rightAnswer1 = "1121"
+const rightAnswer2 = "black"
+const rightAnswer3 = "dinozavri"
+const rightAnswer4 = "shorti"
+
+const checkRightAnswer = (event) => {
+  const buttonText = event.target.textContent;
+  if (buttonText === '1121') {
+    event.target.style.backgroundColor = 'red';
+  }
+};
+
+const handleNextQuestion = () => {
+
+  if(questionIndex < quizData.length -1){
+    setQuestionIndex(questionIndex + 1)
+  }else{
+    alert("quiz finished")
+  }
+
+}
+
  
 
   return (
     <div className='quiz'>
+      <h1>aaaaaaaaa</h1>
         <div className="question">
             <div className="question-header">{questionData.question}</div>
             <div className="question-list">
-            <button className="quest answer-1">{questionData.answers[1]}</button>
-            <button className="quest answer-2">{questionData.rightAnswer}</button>
-            <button className="quest answer-3">{questionData.answers[2]}</button>
-            <button className="quest answer-4">{questionData.answers[3]}</button>
+            <button onClick={checkRightAnswer} className="quest answer-1">{questionData.answers[0]}   </button>
+            <button onClick={checkRightAnswer} className="quest answer-2">{questionData.answers[1]}   </button>
+            <button onClick={checkRightAnswer} className="quest answer-3">{questionData.answers[2]}   </button>
+            <button onClick={checkRightAnswer} className="quest answer-4">{questionData.answers[3]}   </button>
             </div>
             
         </div>
 
-        <button className='finish'>next question</button>
+        <button onClick={handleNextQuestion} className='finish'>{questionIndex < quizData.length -1 ? "Next Question" : "Finish"}</button>
     </div>
   )
 }
