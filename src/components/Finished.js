aaa4ab262ff3da3c7f383e8521b5cc8d93d1d736
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-07-29 21:09:36 +0400

    functionality finished

--- src/components/Finished.js
+++ src/components/Finished.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Finished.css"
 
-export default function Finished({ score, totalQuestions }) {
+export default function Finished({ score, totalQuestions, restart }) {
     return (
         <div className='Finished'>
           <h1>Quiz finished. Your score: {score}/{totalQuestions}</h1>
+
+          <button onClick={restart} >Start again</button>
         </div>
     )
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,48 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import './Quiz.css';
-import Finished from './Finished';
+import React, { useState, useEffect } from "react";
+import "./Quiz.css";
+import Finished from "./Finished";
 
 export default function Quiz() {
   const quizData = [
     {
-      question: 'The Georgian national epic, "The Knight in the Panthers Skin," was written by:',
-      answers: [ ' Vazha-Pshavela', ' Ilia Chavchavadze', 'Shota Rustaveli',  'Nikoloz Baratashvili'],
-      correctAnswer: 'Shota Rustaveli',
+      question:
+        'The Georgian national epic, "The Knight in the Panthers Skin," was written by:',
+      answers: [
+        " Vazha-Pshavela",
+        " Ilia Chavchavadze",
+        "Shota Rustaveli",
+        "Nikoloz Baratashvili",
+      ],
+      correctAnswer: "Shota Rustaveli",
     },
     {
-      question: 'Which Georgian historical figure is associated with the invention of the Georgian alphabet?',
-      answers: ['Saint Nino', 'King Parnavaz', 'King Mirian III', 'King Vakhtang I Gorgasali'],
-      correctAnswer: 'King Parnavaz',
+      question:
+        "Which Georgian historical figure is associated with the invention of the Georgian alphabet?",
+      answers: [
+        "Saint Nino",
+        "King Parnavaz",
+        "King Mirian III",
+        "King Vakhtang I Gorgasali",
+      ],
+      correctAnswer: "King Parnavaz",
     },
     {
-      question: 'In which battle did King David IV (the Builder) defeat the Seljuk Turks, securing Georgian independence?',
-      answers: ['Battle of Didgori', 'Battle of Martqopi', 'Battle of Lake Paravani', 'Battle of Krtsanisi'],
-      correctAnswer: 'Battle of Didgori',
+      question:
+        "In which battle did King David IV (the Builder) defeat the Seljuk Turks, securing Georgian independence?",
+      answers: [
+        "Battle of Didgori",
+        "Battle of Martqopi",
+        "Battle of Lake Paravani",
+        "Battle of Krtsanisi",
+      ],
+      correctAnswer: "Battle of Didgori",
     },
     {
-      question: 'Which Georgian king is often referred to as "George the Brilliant" due to his patronage of the arts and culture?',
-      answers: [ 'Giorgi IV (Lasha)', 'George IX of Kartli', 'George II of Imereti', 'George V of Georgia'],
-      correctAnswer: 'George V of Georgia',
+      question:
+        'Which Georgian king is often referred to as "George the Brilliant" due to his patronage of the arts and culture?',
+      answers: [
+        "Giorgi IV (Lasha)",
+        "George IX of Kartli",
+        "George II of Imereti",
+        "George V of Georgia",
+      ],
+      correctAnswer: "George V of Georgia",
     },
     {
-      question: 'What was the name of the prominent Georgian queen who ruled in the 12th century and played a significant role in expanding Georgias territory?',
-      answers: [ 'Queen Rusudan', 'Queen Tamar',' Queen Ketevan', 'Queen Marta of Margveti'],
-      correctAnswer: 'Queen Tamar',
+      question:
+        "What was the name of the prominent Georgian queen who ruled in the 12th century and played a significant role in expanding Georgias territory?",
+      answers: [
+        "Queen Rusudan",
+        "Queen Tamar",
+        " Queen Ketevan",
+        "Queen Marta of Margveti",
+      ],
+      correctAnswer: "Queen Tamar",
     },
-    
   ];
 
   const [questionIndex, setQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
   const questionData = quizData[questionIndex];
-  const [count, setCount] = useState(0)
-  const [showFinish, setShowFinish] = useState(false)
+  const [count, setCount] = useState(0);
+  const [showFinish, setShowFinish] = useState(false);
   const showFinishButton = count >= 5;
-  
-
 
   const handleAnswerClick = (selectedAnswer) => {
-    setCount(count + 1)
+    setCount(count + 1);
     // Check if the selected answer is correct
     if (selectedAnswer === questionData.correctAnswer) {
       // If the answer is correct, increment the score
@@ -54,44 +81,54 @@ export default function Quiz() {
       setQuestionIndex(questionIndex + 1);
     } else {
       // Quiz finished, display the score
-     
     }
   };
 
   const handleFinishClick = () => {
-    setShowFinish(true)
+    setShowFinish(true);
   };
+
+  const restart = () => {
+
+    setQuestionIndex(0)
+    setScore(0)
+    setCount(0)
+    setShowFinish(false)   
+
+  }
 
   // <Finished score={score} totalQuestions={quizData.length} />
 
   return (
     <div className="quiz">
+      {showFinish ? (
+        <Finished score={score} totalQuestions={quizData.length} restart = {restart}/>) : 
+        
+        (
+        <div>
+          <div className="question">
+            <div className="question-header">{questionData.question}</div>
+            <div className="question-list">
+              {questionData.answers.map((answer, index) => (
+                <button
+                  key={index}
+                  className="quest"
+                  onClick={() => handleAnswerClick(answer)}
+                >
+                  {answer}
+                </button>
+              ))}
+            </div>
+          </div>
 
-      {showFinish ? ( <Finished score={score} totalQuestions={quizData.length} />
-      ) : ( <div>
-         <div className="question">
-        <div className="question-header">{questionData.question}</div>
-        <div className="question-list">
-          {questionData.answers.map((answer, index) => (
-            <button
-              key={index}
-              className="quest"
-              onClick={() => handleAnswerClick(answer)}
-            >
-              {answer}
+          {showFinishButton && (
+            <button onClick={handleFinishClick} className="finish">
+              Finish
             </button>
-          ))}
+          )}
         </div>
-      </div>
-
-      {showFinishButton && (
-        <button onClick={handleFinishClick} className="finish">
-          Finish
-        </button>
       )}
-      </div>)
-    }
- 
+
     </div>
   );
 }
